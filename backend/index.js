5909481e823e95f4cb88e83d02a8aa6625725926
navjotsharma5500 <navjot.sharma@thapar.edu>
2025-11-25 02:12:25 +0530

    Fix upload limit

--- backend/index.js
+++ backend/index.js
@@ -11,8 +11,8 @@ const app = express();
 // -----------------------------
 // Middlewares
 // -----------------------------
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(morgan("dev"));
 
 app.use(
