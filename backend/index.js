6c40123761806fba1838684da493b25d1939e02a
navjotsharma5500 <navjot.sharma@thapar.edu>
2025-11-25 01:59:26 +0530

    Increased JSON upload limit to 5mb for enquiry form

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,8 @@ const app = express();
 // -----------------------------
 // Middlewares
 // -----------------------------
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ limit: "5mb", extended: true }));
 app.use(morgan("dev"));
 
 // CORS Settings
