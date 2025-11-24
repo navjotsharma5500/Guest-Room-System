a294b13e422a7df65442383f545eff8f71e56d26
navjotsharma5500 <navjot.sharma@thapar.edu>
2025-11-25 03:12:36 +0530

    Updated backend to match frontend enquiry structure

--- backend/models/Enquiry.js
+++ backend/models/Enquiry.js
@@ -2,18 +2,42 @@ import mongoose from "mongoose";
 
 const enquirySchema = new mongoose.Schema(
   {
-    guestName: { type: String, required: true },
-    guestEmail: { type: String, required: true },
-    guestPhone: { type: String, required: true },
+    guestName: {
+      type: String,
+      required: true,
+    },
+    guestEmail: {
+      type: String,
+      required: true,
+    },
+    guestPhone: {
+      type: String,
+      required: true,
+    },
 
-    message: { type: String },
-    preferredDate: { type: Date },
+    message: String,
+    preferredDate: String,
+
+    fullData: {
+      rollno: String,
+      department: String,
+      gender: String,
+      from: String,
+      to: String,
+      guests: String,
+      females: String,
+      males: String,
+      state: String,
+      city: String,
+      reference: String,
+      files: [String],
+    },
 
     status: {
       type: String,
-      enum: ["new", "viewed", "converted", "ignored"],
-      default: "new"
-    }
+      enum: ["pending", "approved", "rejected"],
+      default: "pending",
+    },
   },
   { timestamps: true }
 );
