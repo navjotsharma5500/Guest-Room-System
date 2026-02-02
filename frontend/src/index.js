cb1e325bdda56de6527fe7f9a5578db5e7350a5d
navjotsharma5500 <navjot.sharma@thapar.edu>
2026-02-03 03:32:37 +0530

    new dashboard layout 1.9

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,10 @@
-/* eslint-disable import/first */
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { AuthProvider } from "./context/AuthContext";
+import { ToastProvider } from "./context/ToastContext"; 
+import ImageKitProvider from "./providers/ImageKitProvider";
 
 // 🔒 Hide browser console logs in production (frontend only)
 if (process.env.NODE_ENV === "production") {
@@ -8,24 +14,16 @@ if (process.env.NODE_ENV === "production") {
   console.debug = () => {};
 }
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import { AuthProvider } from "./context/AuthContext";
-import { ToastProvider } from "./context/ToastContext"; 
-import ImageKitProvider from "./providers/ImageKitProvider";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     <AuthProvider>
-      <ToastProvider>
+      <ToastProvider>                                  
         <ImageKitProvider>
           <App />
         </ImageKitProvider>
-      </ToastProvider>
+      </ToastProvider>                                  
     </AuthProvider>
   </React.StrictMode>
 );
